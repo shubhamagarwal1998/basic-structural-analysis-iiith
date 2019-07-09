842a840836e1070057f59bf0380702587bb729c9
shubhamagarwal1998 <agarwalpshubham1998@gmail.com>
2019-07-09 23:16:35 +0530

    Iintial commit

---
+++ SRIP/Codes/Cuboid.js
@@ -0,0 +1,22 @@
+function textDisplay1() {
+	var string="<b>All Sides Fixed</b>";
+	document.getElementById("Clicked").innerHTML=string;
+}
+function textDisplay2() {
+	var string="<b>3 Sides Fixed</b>";
+	document.getElementById("Clicked").innerHTML=string;
+}
+function textDisplay3A() {
+	var string="<b>2 Sides Fixed</b><br>Side By Side";
+	document.getElementById("Clicked").innerHTML=string;
+}
+function textDisplay3B() {
+	var string="<b>2 Sides Fixed</b><br>Opposite Sides";
+	document.getElementById("Clicked").innerHTML=string;
+}
+function textDisplay4() {
+	var string="<b>1 Side Fixed</b>";
+	document.getElementById("Clicked").innerHTML=string;
+}
+
+textDisplay1();
