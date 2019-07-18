cbfe9e26d59e3e5c016cd9a20969dc9c4ea97294
shubhamagarwal1998 <agarwalpshubham1998@gmail.com>
2019-07-19 01:25:29 +0530

    Added some code

--- SRIP/Codes/Cuboid.js
+++ SRIP/Codes/Cuboid.js
@@ -2,9 +2,15 @@ function textDisplay1() {
 	var string="<b>All Sides Fixed</b>";
 	document.getElementById("Clicked").innerHTML=string;
 }
+function textDisplay1A() {
+	var string="<b>All Sides Fixed</b>";
+	document.getElementById("Clicked").innerHTML=string;
+	load1();
+}
 function textDisplay2() {
 	var string="<b>3 Sides Fixed</b>";
 	document.getElementById("Clicked").innerHTML=string;
+	load2();
 }
 function textDisplay3A() {
 	var string="<b>2 Sides Fixed</b><br>Side By Side";
